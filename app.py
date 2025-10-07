f73cd66c4f8c9cf32716fcdb86e26d71b379420f
BuilderGames20 <113077427+BuilderGames20@users.noreply.github.com>
2025-10-07 16:46:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,6 @@ def home():
     return render_template("index.html", projetos=projetos)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
